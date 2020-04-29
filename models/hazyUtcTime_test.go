76b66bf3d505197aeefa167a5984845455e8189f
AdamHawtin <adam.hawtin@ons.gov.uk>
2020-04-29 13:28:05 +0100

    Test non zero TZ

--- models/hazyUtcTime_test.go
+++ models/hazyUtcTime_test.go
@@ -8,10 +8,12 @@ import (
 
 func TestUnmarshalJSON(t *testing.T) {
 	expectedTimeUtc, _ := time.Parse("2006-01-02T15:04:05Z07:00", "2008-08-24T00:00:00Z")
+	expectedTimeBst, _ := time.Parse("2006-01-02T15:04:05Z07:00", "2008-08-24T00:00:00+01:00")
 
 	t.Run("Without TZ", testUnmarshalJSON([]byte(`"2008-08-24T00:00:00"`), &expectedTimeUtc))
 	t.Run("With zulu TZ", testUnmarshalJSON([]byte(`"2008-08-24T00:00:00Z"`), &expectedTimeUtc))
-	t.Run("With explicit TZ", testUnmarshalJSON([]byte(`"2008-08-24T00:00:00+00:00"`), &expectedTimeUtc))
+	t.Run("With explicit UTC TZ", testUnmarshalJSON([]byte(`"2008-08-24T00:00:00+00:00"`), &expectedTimeUtc))
+	t.Run("With explicit non zero TZ", testUnmarshalJSON([]byte(`"2008-08-24T00:00:00+01:00"`), &expectedTimeBst))
 }
 
 func testUnmarshalJSON(timeBuf []byte, expectedTime *time.Time) func(t *testing.T) {
@@ -27,9 +29,10 @@ func testUnmarshalJSON(timeBuf []byte, expectedTime *time.Time) func(t *testing.
 			t.Errorf("Expected time: %s, got: %s", expectedTime, hazyUtcTime)
 		}
 
-		actualTz, _ := hazyUtcTime.Zone()
-		if reflect.DeepEqual(0, actualTz) {
-			t.Errorf("Expected UTC TZ, got: %s", actualTz)
+		_, actualTzOffset := hazyUtcTime.Zone()
+		_, expectedTzOffset := expectedTime.Zone()
+		if actualTzOffset != expectedTzOffset {
+			t.Errorf("Expected UTC TZ, got: %d", actualTzOffset)
 		}
 	}
 }
