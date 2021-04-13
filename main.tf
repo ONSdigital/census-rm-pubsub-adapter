35660b84567fcfac6a89e925d85a0ed5947fb725
Apollo Akora <apollo.akora@ext.ons.gov.uk>
2021-04-13 15:20:10 +0100

    Attempting to remove non mandatories

--- main.tf
+++ main.tf
@@ -1,7 +1,6 @@
 provider google {
     project = "census-rm-apolloakora04"
     region  = "europe-west2"
-    zone    = "europe-west2-b"
 }
 
 resource random_id postfix {
@@ -10,8 +9,6 @@ resource random_id postfix {
 
 resource google_storage_bucket cloud-build-bucket-no1 {
     name                        = "my-cloud-build-bucket-${random_id.postfix.hex}"
-    project                     = "census-rm-apolloakora04"
     storage_class               = "REGIONAL"
-    location                    = "europe-west2"
     uniform_bucket_level_access = true
 }
