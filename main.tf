c87e4a14838a51c2f73815c4ddbe5793b97fef98
Apollo Akora <apollo.akora@ext.ons.gov.uk>
2021-04-13 14:20:21 +0100

    tweaked bucket name to make unique

--- main.tf
+++ main.tf
@@ -4,7 +4,7 @@ provider google {
 }
 
 resource google_storage_bucket cloud-build-bucket {
-    name                        = "my-cloud-build-bucket"
+    name                        = "my-cloud-build-trigger-bucket"
     storage_class               = "REGIONAL"
     location                    = "europe-west2"
     uniform_bucket_level_access = true
