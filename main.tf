1b4732b97e5870f85dfa2726563f4a155d7dcd6f
Apollo Akora <apollo.akora@ext.ons.gov.uk>
2021-04-15 14:28:49 +0100

    show n tell

--- main.tf
+++ main.tf
@@ -7,7 +7,7 @@ resource random_id postfix {
     byte_length = 4
 }
 
-resource google_storage_bucket cloud-build-bucket-no1 {
+resource google_storage_bucket cloud-build-bucket-no2 {
     name                        = "my-cloud-build-bucket-${random_id.postfix.hex}"
     storage_class               = "REGIONAL"
     location                    = "europe-west2"
